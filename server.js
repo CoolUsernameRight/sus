33e24fd3ff24e9a3f02feedca85138e41c2984c3
komedy101 <notpyro.lol@gmail.com>
2022-03-19 14:18:59 +0100

    🍑🥵 AAAAAAAAA

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const ex = require('express');
-const path = require('path')
+const path = require('path');
+const videos = require('./public/videos.json');
 const app = ex();
 app.set('views', path.join(__dirname, 'views'));
 app.use(ex.static('public'));
 app.set('view engine', 'ejs');
 app.get('*', (req, res) => {
-  res.status(200).render('home');
+  const video = videos[Math.floor(Math.random()*videos.length)];
+  res.status(200).render('home', { video });
 });
 app.listen(process.env.PORT || 8080);
